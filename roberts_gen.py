5ea167a2a5e01566e2cdef5424b24893f6b66bf1
Angela <acampbe@ksu.edu>
2018-05-08 10:16:02 -0500

    cms link input attempt

--- roberts_gen.py
+++ roberts_gen.py
@@ -16,28 +16,38 @@ enrichments = np.arange(2.0, 4.5+increment, increment)
 # generate shell script that runs all the CASMO inputs generated
 casmo_script = ''
 cmslink_script = ''
+cmslink_pwrm = "'MOX' 'ON' /\n'NEW' 'cms.pwr-all.lib'/\n"
 
 for e in enrichments:
    
-    sim_name = 'NOBP{:.3f}'.format(e).replace('.', 'p')
-    nobp = 'NOBP{:.3f}.inp'.format(e)
+    sim_name_nobp = 'NOBP{0:.3f}'.format(e).replace('.', 'p')
+    nobp = 'NOBP{0:.3f}'.format(e)
     f = open(nobp, 'w')
-    f.write(template_nobp.format(sim_name, e))
+    f.write(template_nobp.format(sim_name_nobp, e))
     f.close()
 
-    sim_name = 'IFBA{:.3f}'.format(e).replace('.', 'p')
-    ifba = 'IFBA{:.3f}.inp'.format(e)
+    sim_name_ifba = 'IFBA{0:.3f}'.format(e).replace('.', 'p')
+    ifba = 'IFBA{0:.3f}'.format(e)
     f = open(nobp, 'w')
-    f.write(template_ifba.format(sim_name, e))
+    f.write(template_ifba.format(sim_name_ifba, e))
     f.close()
 
-    casmo_script += 'casmo4 -k {}\n'.format(nobp)
-    casmo_script += 'casmo4 -k {}\n'.format(ifba)
+    casmo_script += 'casmo4 -k {0}.inp\n'.format(nobp)
+    casmo_script += 'casmo4 -k {0}.inp\n'.format(ifba)
 
-    cmslink_script
+    cmslink_pwrm += "'CAS' '{0}.cax' '{1}'/\n'STA'/\n".format(nobp, sim_name_nobp)
+    cmslink_pwrm += "'CAS' '{0}.cax' '{1}'/\n'STA'/\n".format(ifba, sim_name_ifba)
+		
+    cmslink_script += "cmslink cmspwrm.inp"
 
+cmslink_pwrm += "'END'/"
+	
 f = open('run_casmo.sh', 'w')
 f.write(casmo_script)
+f.close()
+
+f = open('cmspwrm.inp', 'w')
+f.write(cmslink_pwrm)
 f.close()
 
 f = open('run_cmslink.sh', 'w')
